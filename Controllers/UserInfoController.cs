cd7f210d043b5409074badc4323fdc49b4cfcd79
Digambar Patil <mapsdap@gmail.com>
2022-11-02 10:33:45 +0530

    Commit by Digambar

---
+++ Controllers/UserInfoController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using app1.Models;
+
+namespace app1.Controllers;
+
+public class UserInfoController : Controller
+{
+    private readonly ILogger<UserInfoController> _logger;
+
+    public UserInfoController(ILogger<UserInfoController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult UserInfo1()
+    {
+        return View();
+    }
+
+    public IActionResult UserInfo2()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
